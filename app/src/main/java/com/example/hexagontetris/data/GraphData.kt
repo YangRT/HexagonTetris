2b1d6bc8aefca9609aba27961ba0b188c1a09b90
YangRT <1946464079@qq.com>
2022-03-03 12:11:06 +0800

    fea: 新增图形类型基类及相关信息

---
+++ app/src/main/java/com/example/hexagontetris/data/GraphData.kt
@@ -0,0 +1,38 @@
+package com.example.hexagontetris.data
+
+class GraphData {
+
+    companion object {
+        
+        const val COLOR_PURPLE = 0
+        const val COLOR_YELLOW = 1
+        const val COLOR_PINK = 2
+        const val COLOR_ORANGE = 3
+        const val COLOR_GREEN = 4
+        const val COLOR_CYAN = 5
+
+        const val SHAPE_POINT = 0
+
+        const val SHAPE_LINE_LEFT = 1
+        const val SHAPE_LINE_RIGHT = 2
+        const val SHAPE_LINE_HORIZONTAL = 3
+
+        const val SHAPE_ARC_LEFT = 4
+        const val SHAPE_ARC_RIGHT = 5
+        const val SHAPE_ARC_HORIZONTAL = 6
+
+        const val SHAPE_SQUARE_LEFT = 7
+        const val SHAPE_SQUARE_RIGHT = 8
+        const val SHAPE_SQUARE_HORIZONTAL = 9
+
+        const val SHAPE_TOP_TRIANGLE_LEFT = 10
+        const val SHAPE_TOP_TRIANGLE_RIGHT = 11
+        const val SHAPE_TOP_TRIANGLE_HORIZONTAL = 12
+
+        const val SHAPE_BOTTOM_TRIANGLE_LEFT = 10
+        const val SHAPE_BOTTOM_TRIANGLE_RIGHT = 11
+        const val SHAPE_BOTTOM_TRIANGLE_HORIZONTAL = 12
+
+
+    }
+}
---
+++ app/src/main/java/com/example/hexagontetris/weight/Hexagon.kt
@@ -0,0 +1,16 @@
+package com.example.hexagontetris.weight
+
+import android.graphics.Canvas
+
+
+class Hexagon {
+
+    companion object {
+        const val HEXAGON_RADIUS = 20
+        const val HEXAGON_EDGE = 18
+    }
+
+    fun drawHexagon(canvas: Canvas?, centerX: Int, centerY: Int) {
+
+    }
+}
